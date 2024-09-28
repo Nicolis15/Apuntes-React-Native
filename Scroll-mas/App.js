9114ce64350ecf9d694b72422461268282e4bb7e
Nicolis15 <Nicolistrabajo@gmail.com>
2024-09-28 18:28:03 -0500

    FlatList, Scroll y SectionList

--- Scroll-mas/App.js
+++ Scroll-mas/App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LittleLemonHeader from './components/LitlleLemonHeader';
 import MenuItems from './components/MenuItems';
+import LazyScroll from './components/LazyScroll';
+import ListaSecciones from './components/ListaSecciones';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LittleLemonHeader/>
-      <MenuItems/>
+      <ListaSecciones/>
     </View>
   );
 }
--- Scroll-mas/components/MenuItems.js
+++ Scroll-mas/components/MenuItems.js
@@ -1,11 +1,39 @@
-import {View , Text, ScrollView} from 'react-native';
+import {View , Text, ScrollView, StyleSheet} from 'react-native';
+
+const menuItemsToDisplay = [
+    'Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
+  ];
 
 export default function MenuItems(){
     return(
-        <View>
-            <ScrollView>
-                <Text>View Menu</Text>
+        <View style={Styles.contenedor}>
+            <Text style={Styles.Texto1}>View Menu</Text>
+            <ScrollView  indicatorStyle={'white'} style={Styles.Scroll}>
+                <Text style={Styles.Texto2} >{menuItemsToDisplay[0]}</Text>
             </ScrollView>
         </View>
     )
 };
+
+const Styles = StyleSheet.create({
+    contenedor : {
+        flex : 0.75,
+        alignItems : 'center',
+        paddingTop : 20
+    },
+    Texto1 : {
+        color : 'white',
+
+        fontSize : 30
+    },
+    Texto2 : {
+        color : '#F4CE14',
+        fontSize : 24,
+        textAlign: 'center',
+        flexWrap: 'wrap'
+    },
+    Scroll : {
+        width: '100%'
+    }
+
+});
